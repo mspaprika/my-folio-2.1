316ff6b8814459b5b9b350be22457ffc35510bc5
mspaprika <arunepaprika@gmail.com>
2023-03-12 21:32:30 +0000

    first phase finished

--- app/js/index.js
+++ app/js/index.js
@@ -2,6 +2,11 @@ const mybutton = document.getElementsByClassName("btn-bottom")[0];
 const mybutton2 = document.getElementsByClassName("btn-bottom")[1];
 const mybutton3 = document.getElementsByClassName("btn-bottom")[2];
 const mybutton4 = document.getElementsByClassName("btn-bottom")[3];
+const mybutton5 = document.getElementsByClassName("btn-bottom")[4];
+const mybutton6 = document.getElementsByClassName("btn-bottom")[5];
+const mybutton7 = document.getElementsByClassName("btn-bottom")[6];
+const mybutton8 = document.getElementsByClassName("btn-bottom")[7];
+const mybutton9 = document.getElementsByClassName("btn-bottom")[8];
 
 // const mybutton2 = document.getElementById("btn-take-to-top-2");
 
@@ -9,18 +14,33 @@ mybutton.style.display = "none";
 mybutton2.style.display = "none";
 mybutton3.style.display = "none";
 mybutton4.style.display = "none";
+mybutton5.style.display = "none";
+mybutton6.style.display = "none";
+mybutton7.style.display = "none";
+mybutton8.style.display = "none";
+mybutton9.style.display = "none";
 
 window.onscroll = function () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         mybutton.style.display = "block";
         mybutton2.style.display = "block";
         mybutton3.style.display = "block";
         mybutton4.style.display = "block";
+        mybutton5.style.display = "block";
+        mybutton6.style.display = "block";
+        mybutton7.style.display = "block";
+        mybutton8.style.display = "block";
+        mybutton9.style.display = "block";
     } else {
         mybutton.style.display = "none";
         mybutton2.style.display = "none";
         mybutton3.style.display = "none";
         mybutton4.style.display = "none";
+        mybutton5.style.display = "none";
+        mybutton6.style.display = "none";
+        mybutton7.style.display = "none";
+        mybutton8.style.display = "none";
+        mybutton9.style.display = "none";
     }
 };
 
